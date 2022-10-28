4b741c1bf84f302bb6f12d44bf4c5fb6566b63f0
Angie Garzon <angiegarzondv@gmail.com>
2022-10-27 21:51:16 -0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { getLocaleDayNames } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'store';
+
+  getName(): void {
+    console.log("Hiiiiiii!")
+  }
+}
